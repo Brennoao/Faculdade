7acc15b44a59b7f7effa116ca5bc6a252357bf27
rex5212 <xrexv158925lpo@gmail.com>
2022-11-08 11:35:12 -0300

    Atualizado e Funcional

--- Construção-Back-End/Restaurante/database/seeders/07Produto.ts
+++ Construção-Back-End/Restaurante/database/seeders/07Produto.ts
@@ -4,11 +4,11 @@ import Produtos from 'App/Models/Produto'
 export default class extends BaseSeeder {
   public async run () {
     await Produtos.createMany([
-      {nome: "Fanta-Laranja", quantidade: 1, caloria: 500, fornecedorId: 2, valor: 10, tipoId: 6},
-      {nome: "Coca-Cola", quantidade: 1, caloria: 500, fornecedorId: 1, valor: 10, tipoId: 6},
-      {nome: "Sprite", quantidade: 1, caloria: 500, fornecedorId: 3, valor: 10, tipoId: 6},
-      {nome: "Fanta-Uva", quantidade: 1, caloria: 500, fornecedorId: 4, valor: 10, tipoId: 6},
-      {nome: "Suco-Laranja", quantidade: 1, caloria: 500, fornecedorId: 5, valor: 10, tipoId: 5}
+      {nome: "Fanta-Laranja", quantidade: 1, caloria: 500, fornecedoreId: 2, valor: 10, tipoId: 6},
+      {nome: "Coca-Cola", quantidade: 1, caloria: 500, fornecedoreId: 1, valor: 10, tipoId: 6},
+      {nome: "Sprite", quantidade: 1, caloria: 500, fornecedoreId: 3, valor: 10, tipoId: 6},
+      {nome: "Fanta-Uva", quantidade: 1, caloria: 500, fornecedoreId: 4, valor: 10, tipoId: 6},
+      {nome: "Suco-Laranja", quantidade: 1, caloria: 500, fornecedoreId: 5, valor: 10, tipoId: 5}
     ])
     // Write your database queries inside the run method
   }
