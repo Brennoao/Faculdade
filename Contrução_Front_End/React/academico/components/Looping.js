3655e86b797771a53543ba7911d6ed4b3b619473
Brenno Alves <brennoao@outlook.com>
2023-05-25 13:32:14 -0300

    Component-Table=Created

---
+++ Contrução_Front_End/React/academico/components/Looping.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react'
+import Link from 'next/link'
+import { Button } from 'react-bootstrap';
+import { Table } from 'react-bootstrap'
+import { BsTrashFill } from 'react-icons/Bs';
+import { AiFillEdit } from 'react-icons/Ai';
+
+const Looping = (props) => {
+
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        setData(getAll())
+    }, []);                                                                 // ATUALIZA A FUNÇÃO DATA
+
+    function getAll () {
+        return JSON.parse(localStorage.getItem(props.Cursos)) || []
+    }
+
+    function deleteItem(id) {
+        if (confirm('Deseja realmente deletar este item')) {                // CONFIRM = FUNÇÃO DO JAVASCRIPT = DUAS OPÇÕES OK=TRUE CANCELAR=FALSE
+            const cursos = getAll()                                         // PUXA A FUNÇÃO getAll
+            cursos.splice(id, 1)                                            // SPLICE = FUNÇÃO DO JAVASCRIPT = OFERECE 3 PARÂMETROS (DESSE, DELETAR, ALTERAR) 
+            console.log(cursos)
+            window.localStorage.setItem(props.Cursos, JSON.stringify(cursos))
+            setData(cursos)                                                 // ATUALIZA O USESTATE ATUALIZANDO A FUNÇÃO DESEJADA
+        }
+    }
+
+    return (
+        <>
+            <Link href={'/cursos/form'} className='btn btn-danger mb-3'>Novo</Link>
+            <Table striped bordered hover variant="danger">
+                <thead>
+                    <tr>
+                        {props.colSpan ? <th colSpan={props.colSpan}>#</th> : <th>#</th>}
+                        <th>{props.Nome}</th>
+                        <th>{props.Duracao}</th>
+                        <th>{props.Modalidade}</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data === null ? '' :
+                        data.map((item, i) => (
+                            <tr key={i}>
+                                <td style={{ width: '2rem' }}><Link href={'/cursos/' + i} className='btn btn-danger'><AiFillEdit /></Link></td>
+                                <td style={{ width: '2rem' }}><Button variant='danger' onClick={() => deleteItem(i)}><BsTrashFill /></Button></td>
+                                <td style={{ width: '33.33%' }}>{item[props.Nome]}</td>
+                                <td style={{ width: '33.33%' }}>{item[props.Duracao]}</td>
+                                <td style={{ width: '33.33%' }}>{item[props.Modalidade]}</td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </Table>
+        </>
+    )
+}
+
+export default Looping
--- Contrução_Front_End/React/academico/pages/cursos/index.js
+++ Contrução_Front_End/React/academico/pages/cursos/index.js
@@ -1,60 +1,12 @@
+import React from 'react'
 import Pagina from '@/components/Pagina'
-import Link from 'next/link'
-import React, { useEffect, useState } from 'react'
-import { Button } from 'react-bootstrap';
-import { Table } from 'react-bootstrap'
-import { BsTrashFill } from 'react-icons/Bs';
-import { AiFillEdit } from 'react-icons/Ai';
+import Looping from '@/components/Looping';
 
 const index = () => {
 
-    const [data, setData] = useState([]);
-    useEffect(() => {
-        setData(getAll())
-    }, []);                                                                 // ATUALIZA A FUNÇÃO DATA
-
-    function getAll () {
-        return JSON.parse(localStorage.getItem('cursos')) || []
-    }
-
-    function deleteItem(id) {
-        if (confirm('Deseja realmente deletar este item')) {                // CONFIRM = FUNÇÃO DO JAVASCRIPT = DUAS OPÇÕES OK=TRUE CANCELAR=FALSE
-            const cursos = getAll()                                         // PUXA A FUNÇÃO getAll
-            cursos.splice(id, 1)                                            // SPLICE = FUNÇÃO DO JAVASCRIPT = OFERECE 3 PARÂMETROS (DESSE, DELETAR, ALTERAR) 
-            console.log(cursos)
-            window.localStorage.setItem('cursos', JSON.stringify(cursos))
-            setData(cursos)                                                 // ATUALIZA O USESTATE ATUALIZANDO A FUNÇÃO DESEJADA
-        }
-    }
-
-    console.log(data)
-
     return (
         <Pagina titulo='Cursos'>
-            <Link href={'/cursos/form'} className='btn btn-danger mb-3'>Novo</Link>
-            <Table striped bordered hover variant="danger">
-                <thead>
-                    <tr>
-                        <th colSpan={2}>#</th>
-                        <th>Curso</th>
-                        <th>duracao</th>
-                        <th>Modalidade</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {data === null ? '' :
-                        data.map((item, i) => (
-                            <tr key={i}>
-                                <td style={{ width: '2rem' }}><Link href={'/cursos/' + i} className='btn btn-danger'><AiFillEdit /></Link></td>
-                                <td style={{ width: '2rem' }}><Button variant='danger' onClick={() => deleteItem(i)}><BsTrashFill /></Button></td>
-                                <td style={{ width: '33.33%' }}>{item.nome}</td>
-                                <td style={{ width: '33.33%' }}>{item.duracao}</td>
-                                <td style={{ width: '33.33%' }}>{item.modalidade}</td>
-                            </tr>
-                        ))
-                    }
-                </tbody>
-            </Table>
+            <Looping Cursos='cursos' colSpan='2' Nome='nome' Duracao='duracao' Modalidade='modalidade'/>
         </Pagina>
     )
 }
