ba8371f915fa0f14cd453f41ec079b0324f25b94
Brenno Alves <brennoao@outlook.com>
2023-08-23 21:58:36 -0300

    Commit-Day23

--- Programação_Dispositivos_Moveis/Projetos/my-app/App.js
+++ Programação_Dispositivos_Moveis/Projetos/my-app/App.js
@@ -1,13 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
-import MinMax from './components/MinMax';
-import MeuComponente from './components/MeuComponente'
+import MinMax from './components/Semana01/MinMax';
+import MeuComponente from './components/Semana02/MeuComponente'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <MeuComponente />
-      <MinMax min={10} max={22} />
+      {/* <MinMax min={10} max={22} /> */}
     </View>
   );
 }
