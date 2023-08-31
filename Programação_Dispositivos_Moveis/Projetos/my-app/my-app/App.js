7df45ac0238688567c9be5b23b6bb09defe45e59
Brenno Alves <brennoao@outlook.com>
2023-08-30 21:39:10 -0300

    exercicioDinamico

--- Programação_Dispositivos_Moveis/Projetos/my-app/my-app/App.js
+++ Programação_Dispositivos_Moveis/Projetos/my-app/my-app/App.js
@@ -1,14 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
 import MinMax from './components/Semana01/MinMax';
-import MeuComponente from './components/Semana02/MeuComponente'
-import NumeroAleatorio from './components/Semana03/Numero.js'
+import MeuComponente from './components/Semana02/MeuComponente';
+import DigiteSeuNome from './components/semana03/DigiteSeuNome.js';
+import NumeroAleatorio from './components/semana03/NumeroAleatorio';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <MeuComponente />
       {/* <MinMax min={10} max={22} /> */}
+
+      <DigiteSeuNome />
       <NumeroAleatorio min={1} max={60} />
     </View>
   );
