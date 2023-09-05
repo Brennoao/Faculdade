20ea98a38460ee33cd35ba84440376415949a4fa
Brenno Alves <brennoao@outlook.com>
2023-09-05 20:10:52 -0300

    biscoitoSorte

--- Programação_Dispositivos_Moveis/Projetos/my-app/my-app/App.js
+++ Programação_Dispositivos_Moveis/Projetos/my-app/my-app/App.js
@@ -4,15 +4,18 @@ import MinMax from './components/Semana01/MinMax';
 import MeuComponente from './components/Semana02/MeuComponente';
 import DigiteSeuNome from './components/semana03/DigiteSeuNome.js';
 import NumeroAleatorio from './components/semana03/NumeroAleatorio';
+import BiscoitoSorte from './components/Semana05/BiscoitoSorte';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <MeuComponente />
-      {/* <MinMax min={10} max={22} /> */}
+      {/* <MeuComponente />
+      <MinMax min={10} max={22} />
 
       <DigiteSeuNome />
-      <NumeroAleatorio min={1} max={60} />
+      <NumeroAleatorio min={1} max={60} /> */}
+
+      <BiscoitoSorte />
     </View>
   );
 }
