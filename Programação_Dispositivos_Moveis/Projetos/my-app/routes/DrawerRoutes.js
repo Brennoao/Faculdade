0e8e2d321eefdf86eaab24a9d55df31934ff15b8
Brenno Alves <brennoao@outlook.com>
2023-10-30 10:45:43 -0300

    ExerciciosAlunos

--- Programação_Dispositivos_Moveis/Projetos/my-app/routes/DrawerRoutes.js
+++ Programação_Dispositivos_Moveis/Projetos/my-app/routes/DrawerRoutes.js
@@ -4,14 +4,16 @@ import { createDrawerNavigator } from '@react-navigation/drawer'
 
 import Carros from '../screen/Carros/Carros'
 import Test from '../screen/Carros/Test'
+import StackAlunos from '../components/ExeAlunos/StackAlunos'
 
 const Drawer = createDrawerNavigator()
 
 export default function DrawerRoutes() {
     return (
-        <Drawer.Navigator initialRouteName='Carros'>
+        <Drawer.Navigator initialRouteName='StackAlunos'>
             <Drawer.Screen name="Carros" component={Carros} />
             <Drawer.Screen name="Test" component={Test} />
+            <Drawer.Screen name="StackAlunos" component={StackAlunos} />
         </Drawer.Navigator>
 
     )
